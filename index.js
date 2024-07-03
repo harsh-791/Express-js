662c9be8b237b9e7814b7787fdf0013101a53f8e
Harsh Verma <harsh.verma1503@gmail.com>
2024-07-03 10:36:08 +0530

    post request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let courses = [
     {id:1, name: "java"},
@@ -10,8 +11,18 @@ app.get('/courses',(req,res)=>{
     res.json(courses);
 });
 
+// POST request handler for /courses
+app.post('/courses', (req, res) => {
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name : req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http:localhost:${PORT}`);
 });
-// app.listen(PORT);
